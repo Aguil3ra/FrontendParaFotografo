e333d424e9dd3338aa346daada887a9343a87b48
Aguil3ra <mariajoseaguileraaguirre71@gmail.com>
2025-04-10 21:32:26 -0400

    arreglo del llamado css

--- src/pages/EdicionDePerfil/EdicionPerfil.jsx
+++ src/pages/EdicionDePerfil/EdicionPerfil.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import styles from "./EditProfile.module.css";
+import styles from "./EdicionPerfil.css";
 
 const EditProfile = () => {
     const [formData, setFormData] = useState({
